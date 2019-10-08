6f65a27869315d14785244519cfd19dcf48077fc
rilu0361 <nyk.nopo0105ro@outlook.jp>
2019-10-08 16:52:23 +0900

    Deployed 1c066fb with MkDocs version: 1.0.4

--- test.py
+++
@@ -1,6 +0,0 @@
-from keras.preprocessing.image import load_img, save_img, img_to_array, array_to_img
-COLOR = "grayscale"   # "rgb", "rgba", "grayscale"
-IMG_SIZE = (50,50)    # 画像サイズ
-img = load_img("./image/3.png",color_mode=COLOR,target_size=IMG_SIZE)
-img_array = img_to_array(img)
-save_img("./image/new_sample.png", img_array)
